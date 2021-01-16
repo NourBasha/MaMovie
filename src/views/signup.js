4f7373f1900a5469720e5966aa4bd99a4a05263c
NourB <nour.basha2011@gmail.com>
2021-01-16 02:52:32 +0200

    small fix for tightened password, production

--- src/views/signup.js
+++ src/views/signup.js
@@ -120,7 +120,7 @@ const Signup = (props) => {
     
     function validatePassword(password) {
       //eslint-disable-next-line
-      const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+      const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
       return re.test(password);
     }
 
